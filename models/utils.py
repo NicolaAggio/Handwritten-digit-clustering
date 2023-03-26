2a4d376c41b9019e0f94a852a8efbaad4c66f36d
NicolaAggio <880008@stud.unive.it>
2023-03-26 11:10:43 +0200

    update

--- models/utils.py
+++ models/utils.py
@@ -1,3 +1,6 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import random
 
 def correctly_clustered(labels, clusters):
     count = 0
@@ -22,6 +25,29 @@ def rand_index(labels, clusters):
     b = correctly_unclustered(labels, clusters)
     n = len(labels)
     return 2*(a+b)/(n*(n-1))
+
+def plot_clustering(X, labels, cluster_centers=None):
+    labels_unique = np.unique(labels)
+    n_clusters_ = len(labels_unique)
+    
+    plt.figure(figsize=(20,10))
+    plt.clf()
+
+    colors = ["#"+''.join([random.choice('0123456789ABCDEF') for j in range(6)]) for i in range(n_clusters_)]
+    markers = ['.', 'o', 'v', '^', '<', '>', '8', 's', 'p', '*', 'h', 'H', 'D', 'd', 'P', 'X']
+
+    for k, col in zip(range(n_clusters_), colors):
+        my_members = labels == k
+        
+        plt.scatter(X[my_members]["PC_1"], X[my_members]["PC_2"], marker=markers[k%len(markers)], color=col)
+        
+        if cluster_centers is not None:
+            cluster_center = cluster_centers[k]
+            plt.scatter(cluster_center[0],cluster_center[1],marker=markers[k%len(markers)], edgecolor="black", s=150, color =col)
+        
+    plt.title("Estimated number of clusters: %d" % n_clusters_)
+    plt.show()
+
 
 # y = [1,1,1,0,0,0]
 # labels = [1,1,0,0,0,0]
