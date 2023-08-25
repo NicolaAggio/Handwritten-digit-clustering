1cd0b347cf6d0dd48729d4e6efda5165ec5aa710
NicolaAggio <880008@stud.unive.it>
2023-08-25 09:25:41 +0200

    Added plot function

--- plot.py
+++ plot.py
@@ -1,5 +1,9 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import random
+
+from utils import load_PCA_2
+from typing import List
 
 def plot_digits(iter, X, y):
     fig, axs = plt.subplots(10, iter=15)
@@ -12,6 +16,9 @@ def plot_digits(iter, X, y):
             axs[digit,x].axis('off')
 
 def plot_rand_score_vs_PCA(PCA_dimensions, rand_scores):
+    """
+    
+    """
     fig, ax = plt.subplots(figsize=(10,5))
     ax.plot(PCA_dimensions, rand_scores,'-*' , color = 'blue')
     ax.grid(True)
@@ -21,10 +28,48 @@ def plot_rand_score_vs_PCA(PCA_dimensions, rand_scores):
     plt.show()
     
 def plot_training_time_vs_PCA(PCA_dimensions, training_times):
+    """
+    
+    """
     fig, ax = plt.subplots(figsize=(10, 5))
     ax.plot(PCA_dimensions, training_times,'-*' , color = 'red')
     ax.grid(True)
     ax.set_xlabel("PCA dimension")
-    ax.set_ylabel("Training times")
+    ax.set_ylabel("Training time (s)")
     ax.set_title('Training times vs PCA dimension', weight = 'bold')
+    plt.show()
+
+def plot_testing_time_vs_PCA(PCA_dimensions, testing_times):
+    """
+    
+    """
+    fig, ax = plt.subplots(figsize=(10, 5))
+    ax.plot(PCA_dimensions, testing_times,'-*' , color = 'red')
+    ax.grid(True)
+    ax.set_xlabel("PCA dimension")
+    ax.set_ylabel("Testing time (s)")
+    ax.set_title('Testing times vs PCA dimension', weight = 'bold')
+    plt.show()
+
+def plot_clusters(dataset_percentage:float, n_clusters:int, labels:List):
+    """
+    This function plots the clusters obtained by the model with PCA dimension equal to 2.
+
+    INPUT:
+    - 
+    """
+
+    X, _ = load_PCA_2(dataset_percentage)
+        
+    plt.figure(figsize=(20,10))
+    plt.clf()
+
+    colors = ["#"+''.join([random.choice('0123456789ABCDEF') for j in range(6)]) for i in range(n_clusters)]
+
+    for k, col in zip(range(n_clusters), colors):
+        my_members = labels == k
+        
+        plt.scatter(X[my_members]["pca_1"], X[my_members]["pca_2"], color=col) 
+        
+    plt.title("Number of clusters: %d" % n_clusters, weight = "bold")
     plt.show()
--- post_processing.py
+++ post_processing.py
@@ -1,4 +1,12 @@
-from typing import Dict
+import time
+import numpy as np
+
+from utils import load_PCA_test_sets
+from typing import Dict, Union, List
+from sklearn.mixture import GaussianMixture
+from sklearn.cluster import MeanShift, SpectralClustering
+from sklearn.metrics.cluster import rand_score
+from tqdm import tqdm
 
 def best_PCA(tuning_results:Dict[int,tuple]):
     """
@@ -30,7 +38,17 @@ def get_training_times(tuning_results:Dict[int,tuple]):
     
     return training_times
 
-def get_rand_scores(tuning_results:Dict[int,tuple]):
+def get_testing_times(testing_results:Dict[int,tuple]):
+    """
+    This function retrieves, for each PCA dimension value, the testing time of the model fitted with the best values of the hyperparameters.
+    """
+    testing_times = []
+    for pca_dim in testing_results.keys():
+        testing_times.append(testing_results[pca_dim][1])
+    
+    return testing_times
+
+def get_training_rand_scores(tuning_results:Dict[int,tuple]):
     """
     This function retrieves, for each PCA dimension value, the rand scores of the model fitted with the best values of the hyperparameters.
     """
@@ -40,12 +58,76 @@ def get_rand_scores(tuning_results:Dict[int,tuple]):
 
     return rand_scores
 
-def get_n_clusters(tuning_results:Dict[int,tuple]):
+def get_testing_rand_scores(testing_results:Dict[int,tuple]):
+    """
+    This function retrieves, for each PCA dimension value, the rand scores of the model fitted with the best values of the hyperparameters.
+    """
+    rand_scores = []
+    for pca_dim in testing_results.keys():
+        rand_scores.append(testing_results[pca_dim][0])
+
+    return rand_scores
+
+def get_n_clusters_tuning(tuning_results:Dict[int,tuple]):
     """
     This function retrieves, for each PCA dimension value, the number of clusters obtained by the MeanShift model with the best values of the hyperparameters.
     """
     n_clusters = []
     for pca_dim in tuning_results.keys():
-        n_clusters.append(tuning_results[pca_dim[4]])
+        n_clusters.append(tuning_results[pca_dim][4])
 
-    return get_n_clusters
+    return n_clusters
+
+def get_n_clusters_testing(testing_results:Dict[int,tuple]):
+    """
+    This function retrieves, for each PCA dimension value, the number of clusters obtained by the model with the best values of the hyperparameters.
+    """
+    n_clusters = []
+    for pca_dim in testing_results.keys():
+        n_clusters.append(testing_results[pca_dim][2])
+
+    return n_clusters
+
+def get_best_estimators(tuning_results:Dict[int,tuple]):
+    """
+    This function retrieves, for each PCA dimension value, the best estimator obtained in the tuning phase.
+    """
+    best_estimators = []
+    for pca_dim in tuning_results.keys():
+        best_estimators.append(tuning_results[pca_dim][0])
+
+    return best_estimators
+
+def get_labels(testing_results:Dict[int,tuple]):
+    """
+    This function retrieves, for each PCA dimension value, the labels obtained in the testing phase.
+    """
+    labels = []
+    for pca_dim in testing_results.keys():
+        labels.append(testing_results[pca_dim][3])
+
+    return labels
+
+def execute_estimators(best_estimators:List[Union[GaussianMixture, MeanShift, SpectralClustering]], model_name:str, max_pca_dim:int, dataset_percentage:float):
+    """
+    This function execute the best estimator on the test set.
+    """
+    results = {}
+    i = 0
+
+    X_test, y_test = load_PCA_test_sets(max_pca_dim, dataset_percentage)
+
+    for dim in tqdm(X_test.keys(), "Executing " + model_name + " .."):
+        best_estimator = best_estimators[i]
+
+        start = time.time()
+        labels = best_estimator.predict(X_test[dim])
+        testing_time = time.time() - start
+        rand = rand_score(labels, y_test)
+
+        n_clusters = len(np.unique(labels))
+        results[dim] = rand, testing_time, n_clusters, labels
+        
+        i += 1
+    
+    return results
